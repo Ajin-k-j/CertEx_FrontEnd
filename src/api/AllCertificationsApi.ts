39a6a70e1274e9b62833c72de612831aecda4fe7
Ajin-k-j <ajinkjajin@gmail.com>
2024-08-17 14:35:49 +0530

    Revert "Completed the bar graph of all pages"

--- src/api/AllCertificationsApi.ts
+++ src/api/AllCertificationsApi.ts
@@ -4,5 +4,3 @@ export const fetchCertifications = async (): Promise<CertificationData[]> => {
     const response = await apiClient.get<CertificationData[]>("/certifications");
     return response.data;
   };
-
-export { CertificationData };
--- src/api/SimilarCertificationsApi.ts
+++ src/api/SimilarCertificationsApi.ts
@@ -1,8 +1,7 @@
-// src/api/SimilarCertificationsApi.ts
 import apiClient from "./BaseApi";
 import { CertificationData } from '../types/AllCertifications.types';
 
 export const fetchSimilarCertifications = async (): Promise<CertificationData[]> => {
-  const response = await apiClient.get<CertificationData[]>("/similar");
+    const response = await apiClient.get<CertificationData[]>("/similar");
   return response.data;
 };
